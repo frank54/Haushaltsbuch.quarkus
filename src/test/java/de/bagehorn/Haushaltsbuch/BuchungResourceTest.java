cb88521173fc446734ccc96d5c1492b5f6e32849
Frank Bagehorn <frank@bagehorn.de>
2025-04-25 16:32:45 +0200

    Integrationtest

--- src/test/java/de/bagehorn/Haushaltsbuch/BuchungResourceTest.java
+++ src/test/java/de/bagehorn/Haushaltsbuch/BuchungResourceTest.java
@@ -1,7 +1,11 @@
 package de.bagehorn.Haushaltsbuch;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
 import org.junit.jupiter.api.Test;
+
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -9,7 +13,39 @@ import static org.hamcrest.CoreMatchers.is;
 public class BuchungResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
-        given().when().get("/api/v1/buchung").then().statusCode(200).body(is("Hello RESTEasy"));
+    public void shouldGetAlleBuchungen() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).
+        when()
+                .get("/api/v1/buchung").
+        then()
+                .statusCode(200)
+                .body("size()", is(2));
     }
+
+    @Test
+    public void shouldCountAlleBuchungen() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN).
+                when()
+                .get("/api/v1/buchung/count").
+                then()
+                .statusCode(200)
+                .body(is("2"));
+    }
+
+    @Test
+    public void shouldGetBuchung() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParam("id", 1).
+                when()
+                .get("/api/v1/buchung/{id}").
+                then()
+                .statusCode(200)
+                .body("beschreibung", is("Coop"))
+                .body("betrag", is("150.00 SFr"))
+                .body("buchungsdatum", is("2025-03-31"));
+    }
+
 }
