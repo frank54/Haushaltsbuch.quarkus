2131c7101a36d75bc4017665553bcfc3f76c212e
Frank Bagehorn <frank@bagehorn.de>
2025-04-25 16:06:55 +0200

    Ändere betrag auf BigDecimal

--- src/main/java/de/bagehorn/Haushaltsbuch/Buchung.java
+++ src/main/java/de/bagehorn/Haushaltsbuch/Buchung.java
@@ -1,20 +1,19 @@
 package de.bagehorn.Haushaltsbuch;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class Buchung {
     private final int id;
     private String beschreibung;
-    private double betrag;
+    private BigDecimal betrag;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "CET")
     @JsonProperty("buchungsdatum")
     private Date datum;
 
-    public Buchung(int id, String beschreibung, double betrag, Date datum) {
+    public Buchung(int id, String beschreibung, BigDecimal betrag, Date datum) {
         this.id = id;
         this.beschreibung = beschreibung;
         this.betrag = betrag;
@@ -31,13 +30,33 @@ public class Buchung {
         return beschreibung;
     }
 
+    @JsonIgnore
+    public BigDecimal getBetrag() {
+        return betrag;
+    }
+
     @JsonGetter("betrag")
-    public String getBetrag() {
-        return betrag + " SFr";
+    public String getBetragStr() {
+        return betrag.toPlainString() + " SFr";
     }
 
     @JsonGetter("buchungsdatum")
     public Date getDatum() {
         return datum;
+    }
+
+    @JsonSetter("beschreibung")
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    @JsonSetter("betrag")
+    public void setBetrag(BigDecimal betrag) {
+        this.betrag = betrag;
+    }
+
+    @JsonSetter("buchungsdatum")
+    public void setDatum(Date datum) {
+        this.datum = datum;
     }
 }
--- src/main/java/de/bagehorn/Haushaltsbuch/BuchungRepository.java
+++ src/main/java/de/bagehorn/Haushaltsbuch/BuchungRepository.java
@@ -2,6 +2,7 @@ package de.bagehorn.Haushaltsbuch;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -12,8 +13,8 @@ public class BuchungRepository {
     public List<Buchung> getBuchungen() {
         try {
             return List.of(
-                    new Buchung(1, "Coop", 150.0, new SimpleDateFormat("yyyy-MM-dd").parse("2025-03-31")),
-                    new Buchung(2, "Strom", 230.0, new SimpleDateFormat("yyyy-MM-dd").parse("2025-03-25"))
+                    new Buchung(1, "Coop", new BigDecimal("150.00"), new SimpleDateFormat("yyyy-MM-dd").parse("2025-03-31")),
+                    new Buchung(2, "Strom", new BigDecimal("230.00"), new SimpleDateFormat("yyyy-MM-dd").parse("2025-03-25"))
             );
         } catch (ParseException e) {
             throw new RuntimeException(e);
