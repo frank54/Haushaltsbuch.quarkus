d4d3c3737e350a76557f165887af5c8afa5e6ea3
Frank Bagehorn <frank@bagehorn.de>
2025-06-24 21:40:33 +0200

    @TestHTTPEndpoint Annotation im Test

--- src/test/java/de/bagehorn/Haushaltsbuch/resource/BuchungResourceTest.java
+++ src/test/java/de/bagehorn/Haushaltsbuch/resource/BuchungResourceTest.java
@@ -1,6 +1,7 @@
 package de.bagehorn.Haushaltsbuch.resource;
 
 import io.quarkus.test.TestTransaction;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
@@ -11,6 +12,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@TestHTTPEndpoint(BuchungResource.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BuchungResourceTest {
 
@@ -20,7 +22,7 @@ class BuchungResourceTest {
         given()
                 .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).
         when()
-                .get("/api/v1/buchung").
+                .get("/").
         then()
                 .statusCode(200)
                 .body("size()", is(3));
@@ -32,7 +34,7 @@ class BuchungResourceTest {
         given()
                 .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN).
                 when()
-                .get("/api/v1/buchung/count").
+                .get("count").
                 then()
                 .statusCode(200)
                 .body(is("3"));
@@ -45,7 +47,7 @@ class BuchungResourceTest {
                 .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
                 .pathParam("id", 1).
                 when()
-                .get("/api/v1/buchung/{id}").
+                .get("{id}").
                 then()
                 .statusCode(200)
                 .body("beschreibung", is("Coop"))
@@ -60,7 +62,7 @@ class BuchungResourceTest {
                 .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
                 .pathParam("kategorieName","Nahrung / Genuss").
                 when()
-                .get("/api/v1/buchung/kategorie/{kategorieName}").
+                .get("kategorie/{kategorieName}").
                 then()
                 .statusCode(200)
                 .body("size()", is(1));
